4b0b7184c98d6a0b70646b2033bdcf55ecaf44a5
Shaun Tan <shaun.tan21@imperial.ac.uk>
2021-12-23 17:21:09 +0000

    Added libraries and first 3 pre-processing code

--- peermentor_analysis.R
+++ peermentor_analysis.R
@@ -1,1 +1,20 @@
+# Libraries ---------------------------------------------------------------
+library(dplyr)
+library(ggplot2)
+
+
+
+# Data --------------------------------------------------------------------
 peermentor <- read.csv(here::here("data/peermentor.csv"))
+
+# Data Processing ---------------------------------------------------------
+peermentor <- peermentor %>%
+  mutate(gender_factor = ifelse(gender == "male", 0, 1)) # Male = 0, Female = 1
+
+peermentor <- peermentor %>%
+  mutate(region_level = as.factor(region))
+
+peermentor <- peermentor %>%
+  mutate(rehab_success_factor = ifelse(rehab_success == "No", 0, 1)) # No = 0, Yes = 1
+
+
