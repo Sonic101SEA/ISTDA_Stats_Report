8ce00e1d36b77263c964e90bcbb4d84dd30e7bd3
Shaun Tan <shaun.tan21@imperial.ac.uk>
2021-12-23 18:14:13 +0000

    Added injecting status factor code

--- peermentor_analysis.R
+++ peermentor_analysis.R
@@ -12,9 +12,10 @@ peermentor <- peermentor %>%
   mutate(gender_factor = ifelse(gender == "male", 0, 1)) # Male = 0, Female = 1
 
 peermentor <- peermentor %>%
-  mutate(region_level = as.factor(region))
+  mutate(region_factor = as.factor(region))
 
 peermentor <- peermentor %>%
   mutate(rehab_success_factor = ifelse(rehab_success == "No", 0, 1)) # No = 0, Yes = 1
 
-
+peermentor <- peermentor %>%
+  mutate(injecting_status_factor = as.factor(injecting_status))
