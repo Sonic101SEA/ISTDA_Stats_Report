f0f5e7d4c365956dad449e95c444e1db7378332d
Shaun Tan <shaun.tan21@imperial.ac.uk>
2021-12-24 17:00:11 +0000

    Added logistic regression analysis

--- peermentor_analysis.R
+++ peermentor_analysis.R
@@ -10,18 +10,21 @@ peermentor <- read.csv(here::here("data/peermentor.csv"))
 # Data Processing ---------------------------------------------------------
 
 peermentor <- peermentor %>%
-  mutate(gender_factor = ifelse(gender == "male", 0, 1)) # Male = 0, Female = 1
+  # mutate(gender_factor = ifelse(gender == "male", 0, 1)) # Male = 0, Female = 1
+  mutate(gender_factor = as.factor(gender))
 
 peermentor <- peermentor %>%
   mutate(region_factor = as.factor(region))
 
 peermentor <- peermentor %>%
-  mutate(rehab_success_factor = ifelse(rehab_success == "No", 0, 1)) # No = 0, Yes = 1
-  # mutate(rehab_success_factor = as.factor(rehab_success))
+  # mutate(rehab_success_factor = ifelse(rehab_success == "No", 0, 1)) # No = 0, Yes = 1
+  mutate(rehab_success_factor = as.factor(rehab_success))
+
+peermentor <- peermentor %>%
+  mutate(intervention_factor = as.factor(intervention))
 
 
-# Analysis ----------------------------------------------------------------
-## Aim 1: Chi-square testing
+# Analysis Aim 1: Chi-square testing ----------------------------------------------------------------
 # Injecting Status and rehab_success
 # Removing missing in injecting status
 peermentor_2 <- peermentor %>%
@@ -36,12 +39,33 @@ chisq.test(table(peermentor_2$rehab_success_factor,
 
 # Selecting duration use for successful
 duration_use_success <- peermentor %>%
-  filter(rehab_success_factor == 1) %>%
-  select(rehab_success_factor, duration_use)
+  filter(rehab_success_factor == "Yes") %>%
+  select(duration_use)
 
 # Selecting duration use for unsuccessful
 duration_use_failure <- peermentor %>%
-  filter(rehab_success_factor == 0) %>%
-  select(rehab_success_factor, duration_use)
+  filter(rehab_success_factor == "No") %>%
+  select(duration_use)
 
 # 2 sample t-test to see difference
+# Unequal variance
+t.test(duration_use_success, duration_use_failure)
+# Equal variance
+t.test(duration_use_success, duration_use_failure, var.equal = TRUE)
+
+
+# Analysis Aim 2: Logistic Regression -------------------------------------
+peermentor_3 <- peermentor_2 %>%
+  filter(last_obs_outcome == "Study completed")
+
+peermentor_3 <- peermentor_3 %>%
+  mutate(relapse_yes_no = ifelse(is.na(relapse_days), 0, 1)) # 0 = No relapse, 1 = relapse
+
+peermentor_3 <- peermentor_3 %>%
+  filter(housing_status != "Other") %>%
+  mutate(housing_status_factor = as.factor(housing_status))
+
+log_mod1 <- glm(data = peermentor_3, family = binomial(link = logit),
+                relapse_yes_no ~ intervention_factor + age + gender_factor + injecting_status_factor +
+               housing_status_factor + rehab_success_factor)
+summary(log_mod1)
